46a2377681de6c388bd33cd61938c2bd800182e7
Andrei Fidelman <fidelmanlife@gmail.com>
2019-02-15 19:53:43 +0100

    Create Interactive Storybook Stories with the Knobs Decorator

--- src/Button.stories.js
+++ src/Button.stories.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { wInfo } from './utils';
 import { storiesOf } from '@storybook/react';
+import { text, color } from '@storybook/addon-knobs/react';
 import { Button } from './Button';
 
 storiesOf('Button', module)
@@ -11,11 +12,11 @@ storiesOf('Button', module)
     ~~~js
     <button>Click me</button>
     ~~~
-    `)(() => <Button bg="aqua">Hello World</Button>),
+    `)(() => <Button bg={color('bg', 'aqua', 'group1')}>Hello World</Button>),
   )
   .addWithJSX(
     'with background 2',
     wInfo('Description of the Component 2')(() => (
-      <Button bg="yellow">Hello World</Button>
+      <Button bg={text('bg', 'yellow')}>Hello World</Button>
     )),
   );
