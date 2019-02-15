f7b38c319e462610b428caaca585837d7be11632
Andrei Fidelman <fidelmanlife@gmail.com>
2019-02-15 23:50:30 +0100

    Configure React Storybook for use with Typescript

--- src/Button.stories.js
+++ src/Button.stories.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import { wInfo } from './utils';
 import { storiesOf } from '@storybook/react';
-import { text, color } from '@storybook/addon-knobs/react';
 import { Button } from './Button';
+import { text, boolean } from '@storybook/addon-knobs/react';
 
 storiesOf('Button', module)
   .addWithJSX(
     'with background',
     wInfo(`
-    Description of the Component 1
-    ~~~js
-    <button>Click me</button>
-    ~~~
-    `)(() => <Button bg={color('bg', 'aqua', 'group1')}>Hello World</Button>),
+      description
+      
+      ~~~js
+      <Button>slkdjslkdj</Button>
+      ~~~
+      `)(() => <Button bg="palegoldenrod">Hello world</Button>),
   )
-  .addWithJSX(
-    'with background 2',
-    wInfo('Description of the Component 2')(() => (
-      <Button bg={text('bg', 'yellow')}>Hello World</Button>
-    )),
-  );
+  .addWithJSX('with background 2', () => (
+    <Button disabled={boolean('disabled', false)}>
+      {text('text', 'Hello world')}
+    </Button>
+  ));
