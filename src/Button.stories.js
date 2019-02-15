c3401cd6fd8ddeb36b0561ded6bbbc108189f57c
Andrei Fidelman <fidelmanlife@gmail.com>
2019-02-15 19:35:08 +0100

    Apply Style to a React StoryBook with the Info addon to Turn Stories into Documentation

--- src/Button.stories.js
+++ src/Button.stories.js
@@ -1,10 +1,21 @@
 import React from 'react';
-
+import { wInfo } from './utils';
 import { storiesOf } from '@storybook/react';
 import { Button } from './Button';
 
 storiesOf('Button', module)
-  .addWithJSX('with background', () => <Button bg="aqua">Hello World</Button>)
-  .addWithJSX('with background 2', () => (
-    <Button bg="yellow">Hello World</Button>
-  ));
+  .addWithJSX(
+    'with background',
+    wInfo(`
+    Description of the Component 1
+    ~~~js
+    <button>Click me</button>
+    ~~~
+    `)(() => <Button bg="aqua">Hello World</Button>),
+  )
+  .addWithJSX(
+    'with background 2',
+    wInfo('Description of the Component 2')(() => (
+      <Button bg="yellow">Hello World</Button>
+    )),
+  );
